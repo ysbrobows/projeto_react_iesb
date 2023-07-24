a92b262da52423d98ed4c755d56485f1c1ee5e9c
Renan Costa <renancds@hotmail.com>
2023-07-24 20:36:01 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [endereco, setEndereco] = useState('');
+
+  const exibirValores = () => {
+    const mensagem = `Email: ${email}\nNome: ${nome}\nTelefone: ${telefone}\nEndereço: ${endereco}`;
+    alert(mensagem, "Sucesso");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Dados Pessoais</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        onChangeText={(text) => setNome(text)}
+        value={nome}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Telefone"
+        onChangeText={(text) => setTelefone(text)}
+        value={telefone}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Endereço"
+        onChangeText={(text) => setEndereco(text)}
+        value={endereco}
+      />
+      <TouchableOpacity style={styles.button} onPress={exibirValores}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +54,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    margin: 5,
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
---
+++ readme.md
@@ -0,0 +1,8 @@
+# pokedex-fluttler
+project iesb react native
+
+# aluno
+Renan Costa dos Santos
+
+# telas do app
+<img src="https://sat02pap005files.storage.live.com/y4mvw3s4z3jRSaVPi78TLUQxaPzzr_jAQf1Z0vD73pQO5W1grgQkUGHEcf-NKmlinhD6Qge0roRwqH6Abr_tA9k-tofTNQR7iwDlb4IDP18OkDiZ8QPc3IRkWQ7oHwwD0UG3BzCaQpCeTlhoknYWWLwCuA7aLxRkdGP_TtfZ3VLxsirU4ehPu7saZJ-G-M7Vzz3rSpfUh8OgFN999i8YfM3px2DWidBt9qujzR8bl9e78A?encodeFailures=1&width=821&height=860">
